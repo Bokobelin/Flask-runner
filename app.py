aef5b110c8cb5332a457de599bfe63145b5c2c91
Bokobelin <alistair.richelle@gmail.com>
2025-02-28 18:45:16 +0100

    Change port

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Flask is running on Replit!"})
+    return jsonify({"message": "Flask is running on Render!"})
 
 @app.route('/run', methods=['POST'])
 def run_code():
@@ -19,4 +20,5 @@ def run_code():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
